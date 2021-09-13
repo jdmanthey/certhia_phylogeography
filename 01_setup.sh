0478f08cd9763998229d0813b0035365ffc60af3
Joe Manthey <jdmanthey@gmail.com>
2021-09-13 10:13:40 -0500

    Update 01_setup.sh

--- 01_setup.sh
+++ 01_setup.sh
@@ -5,3 +5,11 @@ mkdir 01_bam_files
 mkdir 02_vcf
 mkdir 03_vcf
 mkdir 10_align_script
+mkdir 12_filter
+mkdir 13_ds1_admix50kbp
+mkdir 14_ds2_admix100kbp
+mkdir 15_ds3_trees50kbp
+mkdir 16_ds4_trees100kbp
+mkdir 17_ds5_lostruct
+mkdir 18_ds6_stats50kbp
+mkdir 19_ds7_stats100kbp
