d56238f42c2be2a3f8420f8c9e067619cb8fd03a
Joe Manthey <jdmanthey@gmail.com>
2021-09-14 15:55:31 -0500

    Create 08d_count_sites_in_tree_files.sh

---
+++ 08d_count_sites_in_tree_files.sh
@@ -0,0 +1,12 @@
+for i in $( ls *vcf.gz ); do 
+
+  echo $i
+
+  gunzip -cd $i | grep -v "#" | cut -f 5 >> alt_alleles.txt
+
+done
+
+# count total number of sites
+grep "\\." alt_alleles.txt | wc -l
+# count number of polymorphic sites
+grep -v "\\." alt_alleles.txt | wc -l
