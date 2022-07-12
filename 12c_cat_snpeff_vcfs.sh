64d7874169ca2a05a5233d92ef2d8efefa402909
Joe Manthey <jdmanthey@gmail.com>
2022-07-12 11:37:20 -0500

    Update 12c_cat_snpeff_vcfs.sh

--- 12c_cat_snpeff_vcfs.sh
+++ 12c_cat_snpeff_vcfs.sh
@@ -2,3 +2,7 @@
 grep "^#" Ca_0029_Tg_19.ann.vcf > Certhia_cds_snpeff.vcf
 for i in $( ls *ann2* ); do grep -v "^#" $i >> Certhia_cds_snpeff.vcf; done
 
+# remove sites with warnings from snpEff
+grep -v "WARN" Certhia_cds_snpeff.vcf > Certhia_cds_snpeff2.vcf
+
+# remove the "#" from the header line for reading into R
